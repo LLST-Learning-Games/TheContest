e6af8366c9055d784d37cc45057f619a603c7789
Matthew Stevens <mstevens@llst.ca>
2025-06-22 15:20:47 -0700

    adding very basic room closing

--- Environment/Rooms/Room.cs
+++ Environment/Rooms/Room.cs
@@ -1,15 +1,29 @@
 using Godot;
 using System;
+using System.Linq;
 using Godot.Collections;
 
 public partial class Room : Node2D
 {
-    [Export] private Array<PassageData> _Passages = new Array<PassageData>();
+    [Export] private Array<PassageData> _passages = new Array<PassageData>();
+    [Export] private TileMapLayer _tileMap;
 
+    public Array<PassageData> Passages => _passages;
+    
     public void ClosePassage(PassageDirection directionToClose)
     {
-        var passage = _
+        var passage = _passages.First(x => x.PassageDirection == directionToClose);
+
+        foreach (var directionTile in passage.TileLocations)
+        {
+            _tileMap.SetCell(
+                directionTile,
+                passage.TileSourceId,
+                passage.TileAtlasCoords);
+        }
     }
+    
+    
 }
 
 
--- Environment/Rooms/RoomMap.cs
+++ Environment/Rooms/RoomMap.cs
@@ -1,9 +1,13 @@
+using System;
 using Godot;
 using System.Collections.Generic;
+using System.Linq;
 using Godot.Collections;
 
 public partial class RoomMap : NavigationRegion2D
 {
+    private const float PASSAGE_CLOSE_ODDS = 0.2f;
+    
     [Export] private Vector2I _roomMapSize = new Vector2I(3, 3);
     [Export] private Array<PackedScene> _roomPrefabs;
     [Export] private PackedScene _exitRoomPrefab;
@@ -11,13 +15,19 @@ public partial class RoomMap : NavigationRegion2D
     [Export] private Vector2I _roomSizeInTiles = new Vector2I(24, 13);
     [Export] private int _tileSizeInPixels = 48;
 
-    private List<Room> _rooms;
+    private Room[,] _rooms;
     private readonly RandomNumberGenerator _rng = new();
     
     public override void _Ready()
     {
-        _rooms = new List<Room>(_roomMapSize.X * _roomMapSize.Y);
+        _rooms = new Room[_roomMapSize.X,_roomMapSize.Y];
         
+        GenerateRooms();
+        ClosePassages();
+    }
+
+    private void GenerateRooms()
+    {
         Vector2I exitPosition = new Vector2I(
             _rng.RandiRange(0,_roomMapSize.X - 1), 
             _rng.RandiRange(0,_roomMapSize.Y - 1));
@@ -41,10 +51,110 @@ public partial class RoomMap : NavigationRegion2D
                 GD.Print($"[{GetType().Name}] Placing room of type {newRoom.Name} at position ({x}, {y})");
                 
                 AddChild(newRoom);
-                _rooms.Add(newRoom);
+                _rooms[x,y] = newRoom;
                 newRoom.Position = GetRoomPosition(x, y);
             }
-        }    
+        }
+    }
+
+    private void ClosePassages()
+    {
+        for (int x = 0; x < _roomMapSize.X; x++)
+        {
+            for (int y = 0; y < _roomMapSize.Y; y++)
+            {
+                var room = _rooms[x,y];
+                CloseMapEdges(room,x,y);
+                CloseRandomPassages(room,x,y);
+            }
+        }
+    }
+    
+    private void CloseMapEdges(Room newRoom, int x, int y)
+    {
+        if (x == 0)
+        {
+            newRoom.ClosePassage(PassageDirection.West);
+        }
+        else if (x == _roomMapSize.X - 1)
+        {
+            newRoom.ClosePassage(PassageDirection.East);
+        }
+
+        if (y == 0)
+        {
+            newRoom.ClosePassage(PassageDirection.North);
+        }
+        else if (y == _roomMapSize.Y - 1)
+        {
+            newRoom.ClosePassage(PassageDirection.South);
+        }
+    }
+
+    
+    private void CloseRandomPassages(Room newRoom, int x, int y)
+    {
+        bool shouldClose;
+        foreach(var direction in Enum.GetValues(typeof(PassageDirection)).Cast<PassageDirection>())
+        {
+            shouldClose = _rng.Randf() < PASSAGE_CLOSE_ODDS;
+            if (!shouldClose)
+            {
+                return;
+            }
+            
+            GD.Print($"[{GetType().Name}] Closing passage {direction.ToString()} in room at ({x},{y})");
+            newRoom.ClosePassage(direction);
+            
+            CloseAdjoiningRoom(direction, x, y);
+        }
+    }
+
+    private void CloseAdjoiningRoom(PassageDirection direction, int x, int y)
+    {
+        switch (direction)
+        {
+            case PassageDirection.West:
+            {
+                if (x == 0)
+                {
+                    break;
+                }
+                var room = _rooms[x - 1,y];
+                room.ClosePassage(PassageDirection.East);
+                break;
+            }
+            case PassageDirection.East:
+            {
+                if (x == _roomMapSize.X - 1)
+                {
+                    break;
+                }
+                var room = _rooms[x + 1,y];
+                room.ClosePassage(PassageDirection.West);
+                break;
+            }
+            case PassageDirection.North:
+            {
+                if (y == 0)
+                {
+                    break;
+                }
+                var room = _rooms[x,y - 1];
+                room.ClosePassage(PassageDirection.South);
+                break;
+            }
+            case PassageDirection.South:
+            {
+                if (y == _roomMapSize.Y - 1)
+                {
+                    break;
+                }
+                var room = _rooms[x,y + 1];
+                room.ClosePassage(PassageDirection.North);
+                break;
+            }
+        }
     }
 
     public Vector2I GetRoomPosition(int x, int y)
